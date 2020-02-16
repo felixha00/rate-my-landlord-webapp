0fb1547a9411b8b29d70951c6da1309330ddbb70
Felix <felixha525@gmail.com>
2020-02-15 21:52:18 -0500

    u

--- js/gmaps.js
+++ js/gmaps.js
@@ -4,6 +4,22 @@ function initMap() {
     center: { lat: 56.1304, lng: -106.3468},
     zoom: 8
   });
+
+  var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+
+var docx = sentiment.analyze("I like apples");
+console.log(docx);
+
+// Applying to An Array
+var mydocx = ["I love apples","I don't eat pepper","the movie was very nice","this book is the best"]
+
+mydocx.forEach(function(s){
+	console.log(sentiment.analyze(s));
+})
 }
+
+
+
 
 
