9e36ba09939d1bf9da0ed797c80178615c405b89
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-02-15 20:23:34 -0800

    modified app.js

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.get("/about",function(req,res){
   res.sendFile(path.join(__dirname+"/about.html"))
 })
 app.get("/landlord/new", function(req, res){
-  res.render("./landlords/new.html")
+  res.sendFile(path.join(__dirname+"/views/landlords/add_review.html"))
 })
 app.post("/landlords", function(req, res){
-  var landlord = req.body.landlord
-  var address = req.body.address
+  var name = req.body.landlord
+  var review = req.body.review
   geocoder.geocode(req.body.location, function (err, data) {
     if (err || !data.length) {
       req.flash('error', 'Invalid address');
@@ -45,13 +45,23 @@ app.post("/landlords", function(req, res){
     var lat = data[0].latitude;
     var lng = data[0].longitude;
     var location = data[0].formattedAddress;
-    var newLandlord = {name: name, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
+    var newLandlord = {name: name, address: location, lat: lat, lng: lng};
     // Create a new Landlord and save to DB
     Landlord.create(newLandlord, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
             //redirect back to Landlords page
+            Review.create(review, function(err, newReview){
+              if(err){
+                console.log(err)
+              }
+              else{
+                console.log(newReview)
+                newlyCreated.reviews.push(newReview)
+                newlyCreated.save()
+              }
+            })
             console.log(newlyCreated);
             res.redirect("/landlords");
         }
